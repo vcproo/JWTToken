69b7ae2a4dbd72bd3aca6f19db9ae707007b315a
han <hants666@163.com>
2023-04-17 17:21:49 +0800

    secend main

--- jwt.php
+++ jwt.php
@@ -5,7 +5,7 @@
  * @LastEditors: han hants666@163.com
  * @LastEditTime: 2023-04-17 13:55:24
  * @FilePath: \JWTtoken\jwt.php
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE1
  */
 class Jwt {
  
--- token.php
+++ token.php
@@ -5,7 +5,7 @@
  * @LastEditors: han hants666@163.com
  * @LastEditTime: 2023-04-17 11:44:34
  * @FilePath: \JWTtoken\token.php
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE2
  */
 include 'jwt.php';
 class Token{
